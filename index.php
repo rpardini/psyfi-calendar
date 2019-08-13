b20a9ce4c0effabb5b9ebdccf214cc5cf7b91a4f
ricardop <ricardo.pardini@me.com.br>
2019-08-14 00:14:00 +0200

    work by @dine - mobile

--- index.php
+++ index.php
@@ -11,7 +11,6 @@ $allStages = getAllStages($allActs);
 
 ob_clean();
 
-
 if (@strlen($_REQUEST['stage']) > 1) {
     emitIcalForEvents($allActs, $_REQUEST['stage']);
 } else {
@@ -28,7 +27,6 @@ if (@strlen($_REQUEST['stage']) > 1) {
         <meta charset="UTF-8">
         <meta name="viewport"
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-
         <meta property="og:url" content="<?= $baseUrl ?>"/>
         <meta property="og:type" content="article"/>
         <meta property="og:title" content="Psy-Fi 2019 Timetables and Calendars"/>
@@ -38,66 +36,16 @@ if (@strlen($_REQUEST['stage']) > 1) {
         <meta property="og:image:width" content="876"/>
         <meta property="og:image:height" content="479"/>
 
-
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
         <title>Psy-Fi 2019 timetable // calendar</title>
-        <style type="text/css">
-            body {
-                font-family: 'Roboto', sans-serif;
-                background-color: black;
-                color: white;
-                margin: 0;
-                padding: 0;
-            }
 
-            a {
-                color: aqua;
-            }
+        <link href="https://fonts.googleapis.com/css?family=Barlow&display=swap" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="styles.css">
 
-            .showoff {
-                max-width: 1500px;
-                width: 90%;
-                border-radius: 1em;
-                border: 2px solid darkgray;
-            }
-
-            td.url {
-                font-family: 'Roboto Condensed', sans-serif;
-            }
-
-            table.nowtable {
-                font-family: 'Roboto Condensed', sans-serif;
-                border: 1px solid gray;
-            }
-
-            table.nowtable thead {
-                font-weight: bold;
-            }
-
-            table.nowtable td.stageline {
-                color: yellow;
-                word-break: break-all;
-            }
-
-            span.actname {
-                font-weight: bolder;
-                word-break: break-word;
-            }
-
-            span.acttime {
-                font-style: normal;
-                word-break: break-word;
-            }
-
-            .logo {
-                background-color: black;
-                border-radius: 3em;
-            }
-        </style>
         <script type="text/javascript">
             WebFontConfig = {
-                google: {families: ['Roboto', 'Roboto Condensed']}
+                google: {families: ['Barlow', 'Spinnaker']}
             };
             (function () {
                 var wf = document.createElement('script');
@@ -113,138 +61,116 @@ if (@strlen($_REQUEST['stage']) > 1) {
     </head>
     <body>
 
-    <img align="right" lazy="true" src="img/logo.webp" width="255" height="135" class="logo"/>
+    <header>
+        <div class="logo"><img lazy="true" src="img/logo.webp" width="255" height="135"/></div>
+    </header>
 
-    <h2>Psy-Fi 2019 timetable // calendar</h2>
+    <section id="timetable">
+        <h2>Timetable</h2>
 
-
-    <h3>Right now: <?= $curTS_fmt ?></h3>
-
-    <table class="nowtable" border="2" cellpadding="3" cellspacing="0">
-        <thead>
-        <tr>
-            <td>Stage</td>
-            <td>Now</td>
-            <td>Next</td>
-        </tr>
-        </thead>
-        <tbody>
+        <div class="content">
+            <p>Right now: <?= $curTS_fmt ?></p>
+        </div>
 
         <?php
         foreach ($status as $stage => $data) {
             ?>
-            <tr>
-                <td class="stageline"><?= $stage ?></td>
-                <?= show3Data($data['now']) ?>
-                <?= show3Data($data['next']) ?>
-            </tr>
+            <dl>
+                <dt>Stage</dt>
+                <dd><?= $stage ?></dd>
+                <dt>Now</dt>
+                <dd><?= show3Data($data['now']) ?></dd>
+                <dt>Next</dt>
+                <dd><?= show3Data($data['next']) ?></dd>
+            </dl>
             <?php
         }
-
         ?>
+    </section>
 
-        </tbody>
-    </table>
+    <section id="weather">
+        <h2>Weather</h2>
+        <div class="weather">
+            <iframe src="https://gadgets.buienradar.nl/gadget/weathersymbol" noresize scrolling=no hspace=0 vspace=0 frameborder=0 marginheight=0 marginwidth=0 width=50 height=40></iframe>
+        </div>
+        <div class="content">
+            <a class="button" href="https://www.buienalarm.nl/leeuwarden-frysl%C3%A2n-nederland/53.2166,5.88365">
+                Check weather for the next hours
+            </a>
+        </div>
+    </section>
 
-    <!--    <h2>Weather</h2>
-
-        <iframe lazy="true" style="border-radius: 2em"
-                src="https://gadgets.buienradar.nl/gadget/zoommap/?lat=53.21548&lng=5.87248&overname=2&zoom=11&naam=8926XK&size=2b&voor=0"
-                noresize scrolling=no width=330 height=330 frameborder=no></iframe>
-
-        <iframe lazy="true" src="//gadgets.buienradar.nl/gadget/forecastandstation/6270/" noresize scrolling=no hspace=0
-                vspace=0 style="border-radius: 2em"
-                frameborder=0 marginheight=0 marginwidth=0 width=300 height=190></iframe>
-
-    -->
-
-    <hr/>
-
-    <h3>Calendars</h3>
-
-    <table border="1" cellpadding="2" cellspacing="0">
-        <thead>
-        <tr>
-            <td>Stage</td>
-            <td>Add to Google</td>
-            <td>iCal/Outlook</td>
-            <td>URL</td>
-        </tr>
-        </thead>
-        <tbody>
+    <section id="calendars">
+        <h2>Individual Calendars</h2>
 
         <?php
-
         foreach ($allStages as $stage) {
             $url = "{$baseUrl}?stage=" . urlencode($stage);
             $webCal = str_replace("https://", "webcal://", $url) /*. "&added_inside=" . time()*/
             ;
             ?>
-            <tr>
-                <td><?= $stage ?></td>
-                <td>
-                    <a href="https://www.google.com/calendar/render?cid=<?= urlencode($webCal) ?>&added_at=<?= time() ?>"
-                       target="gcalendar"><?= "Add $stage to Google Calendar" ?></a></td>
-                <td><a href="<?= $url ?>"><?= $stage ?></a></td>
-                <td class="url"><?= $url ?></td>
-            </tr>
+            <dl class="dl--wide">
+                <dt>Stage</dt>
+                <dd><?= $stage ?></dd>
+                <dt>Google</dt>
+                <dd><a class="button" href="https://www.google.com/calendar/render?cid=<?= urlencode($webCal) ?>&added_at=<?= time() ?>" target="gcalendar">Add</a></td></dd>
+                <dt>iCal/Outlook</dt>
+                <dd><a class="button" href="<?= $url ?>">Add</a></dd>
+            </dl>
             <?php
         }
         ?>
-        </tbody>
-    </table>
+    </section>
 
-    <?php
-    $url = "{$baseUrl}?stage=" . urlencode("ALL");
-    ?>
-    <br/><br/>
-    Also, there's this version with all stages in a single calendar: <a href="<?php echo $url ?>">All stages (bit
-        confusing)</a>.
+    <section id="text">
+        <h2>Adding to your own calendar</h2>
+        <div class="content">
+            <p>Above are links to each stage's timetable in iCal (ICS) format.</p>
+            <p>You can import it in any calendar app you want.</p>
+            <p>I recommend adding each stage as a separate calendar (so each has its own color etc), but there is also an <a href="#single">all-stages calendar</a> which is quite messy.</p>
 
-    <hr/>
+            <h3>Google Calendar</h3>
+            <p>Click on the "add" of the corresponding stage. Each time, a new tab/window will open in Google Calendar, and you just click "Yes" or "Add" in the confirmation dialog. Google will take a few seconds after that to display the calendar.</p>
+            <p>It will auto-update every 60 minutes, so you're always set!</p>
+            <p>Unfortunately Google will take a while to read the calendar's Title, and will instead show the webcal://xxx URL for a while. It should fix itself in a few hours.</p>
 
-    <h2>Adding to your own calendar</h2>
-    Above are links to each stage's timetable in iCal (ICS) format.
-    You can import it in any calendar app you want.
-    I recommend adding each stage as a separate calendar (so each has its own color etc), but there is also an all-stage
-    calendar which is quite messy.
+            <h3>Outlook</h3>
+            <p>Just click on the links above; download the ICS file and import into Outlook.</p>
+            <p>Or, use the "Add Calendar" &gt; "From Internet" and paste the URL from the link above (click and hold to copy); this way it will auto-update as well.</p>
 
-    <h4>Google Calendar</h4>
-    Click on the column "Add to Google" above, one for each stage. Each time, a new tab/window will open in Google
-    Calendar, and you just click "Yes" or "Add" in the confirmation dialog. Google will take a few seconds after that to
-    display the calendar.
-    It will auto-update every 60 minutes, so you're always set!
-    Unfortunately Google will take a while to read the calendar's Title, and will instead show the webcal://xxx URL for
-    a while. It should fix itself in a few hours.
+            <h3>It will look like this...</h3>
 
-    <h4>Outlook</h4>
-    Just click on the links above; download the ICS file and import into Outlook.<br/>
-    Or, use the "Add Calendar" &gt; "From Internet" and paste the URL from the list above; this way it will auto-update
-    as well.
+            <img lazy="true" src="img/outlook-min.png"/>
+            <img lazy="true" src="img/gcal-min.png"/>
+        </div>
+    </section>
 
-    <h2>Backing Data</h2>
-    All the backing data is stored in ClashFinder.
-    Thanks to the people who started it and keep it updated.
-    Check it out: <a href="https://clashfinder.com/s/psyfiseedsofscience/" target="clashfinder">Psy Fi Seeds of Science
-        Holland 2019 Clashfinder</a>. You can also check all the <a
-            href="https://clashfinder.com/l/psyfiseedsofscience/?revs" target="clashfinder">changes</a> made over time.
+    <section id="clashfinder">
+        <h2>Backing Data</h2>
+        <div class="content">
+            <p>All the backing data is stored in ClashFinder.</p>
+            <p>Thanks to the people who started it and keep it updated.</p>
+            <p>Check it out: <a href="https://clashfinder.com/s/psyfiseedsofscience/" target="clashfinder">Psy Fi Seeds of Science Holland 2019 Clashfinder</a>.</p>
+            <p>You can also check all the <a  href="https://clashfinder.com/l/psyfiseedsofscience/?revs" target="clashfinder">changes</a> made over time.</p>
+        </div>
 
 
-    <hr/>
+    </section>
 
-    <h2>It will look like this...</h2>
+    <section id="single">
+        <h2>All-stages calendar</h2>
 
-    <img lazy="true" src="img/outlook-min.png" class="showoff"/>
-    <img lazy="true" src="img/gcal-min.png" class="showoff"/>
+        <div class="content">
+            <?php
+            $url = "{$baseUrl}?stage=" . urlencode("ALL");
+            ?>
+            <p>Also, there's this version with all stages in a single calendar: <a href="<?php echo $url ?>">All stages (bit confusing)</a>.</p>
+        </div>
+    </section>
 
-
-    <br/>
-    <br/>
-    <hr>
-    <br/>
-    <br/>
-    by <a href="mailto:ricardo@pardini.net">rpardini</a>
-
+    <footer>
+        by <a href="mailto:ricardo@pardini.net">rpardini</a> & <a href="mailto:dine@dine.tk">dine</a> 💚️
+    </footer>
 
     </body>
     </html>
@@ -253,7 +179,7 @@ if (@strlen($_REQUEST['stage']) > 1) {
 
 function show3Data($act) {
     if (!$act) {
-        return "<td><i>nothing right now</i></td>";
+        return "--";
     }
 
     return "<td>"
